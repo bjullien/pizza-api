bdc6356bcd6b5c7702403d17946ea20255799b42
Bruno Jullien <bruno.jullien@gmail.com>
2019-08-29 16:08:46 -0700

    reorganize code part 1

--- api.js
+++ api.js
@@ -1,14 +1,10 @@
 'use strict'
 const Api = require('claudia-api-builder')
+const getPizzas = require('./handlers/get-pizzas')
 const api = new Api()
 
 api.get('/pizzas', () => {
-    return [
-        'Capricciosa',
-        'Quattro Formaggi',
-        'Napoletana',
-        'Margherita'
-    ]
+  return ['Capricciosa', 'Quattro Formaggi', 'Napoletana', 'Margherita']
 })
 
 module.exports = api
