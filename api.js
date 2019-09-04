a432091762cdced833a16f15975144962cbb96e7
Bruno Jullien <bruno.jullien@gmail.com>
2019-09-04 09:32:41 -0700

    2.1.2 Structuring your API

--- api.js
+++ api.js
@@ -1,10 +1,24 @@
 'use strict'
 const Api = require('claudia-api-builder')
-const getPizzas = require('./handlers/get-pizzas')
 const api = new Api()
+const getPizzas = require('./handlers/get-pizzas')
+
+api.get('/', () => 'Welcome to Pizza API')
 
 api.get('/pizzas', () => {
-  return ['Capricciosa', 'Quattro Formaggi', 'Napoletana', 'Margherita']
+  return getPizzas()
 })
 
+api.get(
+  '/pizzas/{id}',
+  request => {
+    return getPizzas(request.pathParams.id)
+  },
+  {
+    error: 404,
+  }
+)
+
 module.exports = api
+
+// '/pizzas/:id' in Express.js
